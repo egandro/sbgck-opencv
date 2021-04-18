45bd1f4a2fffd53a163daa90832dee682d003523
Harald Fielker <harald.fielker@gmail.com>
2021-04-18 22:24:46 +0200

    .

--- build-opencv.cmd
+++ build-opencv.cmd
@@ -1,17 +1,17 @@
 
+rem run this as elevated powershell
 rem Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
+rem close the powershell / open elevated powershell or cmd and run all this cmds
+
 rem choco install -y 7zip
-
 rem choco install -y git
-
 rem choco install -y cmake.install --installargs '"ADD_CMAKE_TO_PATH=System"'
-
 rem choco install -y python3
-
 rem choco install -y visualstudio2019community
-
 rem choco install -y visualstudio2019-workload-nativedesktop
+rem
+rem ^^^ you might need to reboot Windows at this point
 
 git clone https://github.com/opencv/opencv.git
 git clone https://github.com/opencv/opencv_contrib.git
