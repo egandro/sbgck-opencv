81290f555f0d597a323ca234f4cc2a91c85d33b0
Harald Fielker <harald.fielker@gmail.com>
2021-04-25 22:42:04 +0200

    .

--- tests/test_roi.cpp
+++ tests/test_roi.cpp
@@ -26,30 +26,37 @@ int main(int, char **)
 
   std::string jsonStr =
       "{"
-      "  \"map\": ["
-      "    {"
-      "      \"shape\": \"rect\","
-      "      \"coords\": \"131,120,378,358\","
-      "      \"area\": \"#rect\""
-      "    },"
-      "    {"
-      "      \"shape\": \"circle\","
-      "      \"coords\": \"1155,224,125\","
-      "      \"area\": \"#circle\""
-      "    },"
-      "    {"
-      "      \"shape\": \"poly\","
-      "      \"coords\": \"125,442,24,620,109,851,410,853,557,603,354,416\","
-      "      \"area\": \"#convex\""
-      "    },"
-      "    {"
-      "      \"shape\": \"poly\","
-      "      \"coords\": \"744,719,849,658,1045,556,1237,650,1429,806,1480,412,1028,375,801,161,860,463,515,384\","
-      "      \"area\": \"#concave\""
-      "    }"
-      "  ],"
-      "  \"name\": \"board\""
+      "    \"map\": ["
+      "      {"
+      "        \"shape\": \"rect\","
+      "        \"coords\": \"56,35,303,273\","
+      "        \"area\": \"#rect\""
+      "      },"
+      "      {"
+      "        \"shape\": \"circle\","
+      "        \"coords\": \"1075,159,125\","
+      "        \"area\": \"#circle\""
+      "      },"
+      "      {"
+      "        \"shape\": \"poly\","
+      "        \"coords\": \"139,367,38,545,175,630,384,642,454,524,368,341\","
+      "        \"area\": \"#convex\""
+      "      },"
+      "      {"
+      "        \"shape\": \"poly\","
+      "        \"coords\": \"583,688,649,543,805,425,955,458,1046,684,1183,386,839,320,601,46,660,348,425,239\","
+      "        \"area\": \"#concave\""
+      "      }"
+      "    ],"
+      "    \"name\": \"board\""
       "}";
+
+  Point rectInside(180, 155);
+  Point polyConvexInside(270, 560);
+  Point polyConcaveInside(740, 370);
+  Point polyConcaveOutside(830, 560);
+  Point circleInside(1080, 160);
+  Point outside(1200, 660);
 
   // LOGCFG.prefix = (char *)"test_asset";
   // LOGCFG.headers = true;
