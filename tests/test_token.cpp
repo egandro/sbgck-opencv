6c2b30c7d5f86f47d1a9ac8a29b71a6b3152460a
Harald Fielker <harald.fielker@gmail.com>
2021-04-30 10:37:27 +0200

    .

--- tests/test_token.cpp
+++ tests/test_token.cpp
@@ -6,6 +6,19 @@
 
 structlog LOGCFG = {};
 
+/// https://stackoverflow.com/questions/40895785/using-opencv-to-overlay-transparent-image-onto-another-image
+void drawTransparency(Mat frame, Mat transp, int xPos, int yPos) {
+   // Mat overlay = cv::imread("dice.png", IMREAD_UNCHANGED);
+    Mat mask;
+    vector<Mat> layers;
+
+    split(transp, layers); // seperate channels
+    Mat rgb[3] = { layers[0],layers[1],layers[2] };
+    mask = layers[3]; // png's alpha channel used as mask
+    merge(rgb, 3, transp);  // put together the RGB channels, now transp insn't transparent
+    transp.copyTo(frame.rowRange(yPos, yPos + transp.rows).colRange(xPos, xPos + transp.cols), mask);
+}
+
 void testBackgroundRemoval(string boardEmptyFileName, string frameBoardEmptyFileName)
 {
   SBGCK_TEST_BEGIN("testBackgroundRemoval");
@@ -41,24 +54,31 @@ void testBackgroundRemoval(string boardEmptyFileName, string frameBoardEmptyFile
   SBGCK_TEST_END();
 }
 
-void testExtractTokenFromFrame(string boardEmptyFileName, string frameBoardWithTokenFileName)
+void testExtractTokenFromFrame(string boardEmptyFileName, string frameBoardEmptyFileName, string tokenFileName, int x, int y)
 {
   SBGCK_TEST_BEGIN("testExtractTokenFromFrame");
 
   Board board;
   board.frameBoardEmpty = imread(boardEmptyFileName, IMREAD_COLOR);
-  Mat frameBoardWithToken = imread(frameBoardWithTokenFileName, IMREAD_COLOR);
+
+  // put the token on the frame at the given position
+  Mat frame = imread(frameBoardEmptyFileName, IMREAD_COLOR);
+  Mat transp = imread(tokenFileName, IMREAD_UNCHANGED); // we need the transparency here
+  drawTransparency(frame, transp, x, y);
+
+  imshow("frame", frame);
+  waitKey();
 
   SBGCK_ASSERT_THROW(board.frameBoardEmpty.size().width != 0);
-  SBGCK_ASSERT_THROW(frameBoardWithToken.size().width != 0);
+  SBGCK_ASSERT_THROW(frame.size().width != 0);
 
-  Mat diff = ImageDiff::removeBackground(frameBoardWithToken, board.frameBoardEmpty);
+  Mat diff = ImageDiff::removeBackground(frame, board.frameBoardEmpty);
 
   SBGCK_ASSERT_THROW(board.frameBoardEmpty.size().width == diff.size().width);
   SBGCK_ASSERT_THROW(board.frameBoardEmpty.size().height == diff.size().height);
 
   // imshow("frameBoardEmpty", board.frameBoardEmpty);
-  // imshow("frameBoardWithToken", frameBoardWithToken);
+  // imshow("frame", frame);
   // imshow("diff", diff);
   // waitKey();
 
@@ -81,21 +101,20 @@ void testExtractTokenFromFrame(string boardEmptyFileName, string frameBoardWithT
   SBGCK_TEST_END();
 }
 
+
 int main(int, char **)
 {
   SBGCK_TEST_INIT();
 
   // board
   string boardEmpty_png = CMAKE_SOURCE_DIR + string("/tests/images/board.png");
-  // in production we do this in the calibration step - here we use the same board
   string frameEmpty_png = CMAKE_SOURCE_DIR + string("/tests/images/board.png");
-  // in production we do this in the calibration step - here we use the same board
-  string frame_token_bridge_png = CMAKE_SOURCE_DIR + string("/tests/images/frame_token_bridge.png");
+  string token_red_transparent_bg = CMAKE_SOURCE_DIR + string("/tests/images/token_red_transparent_bg.png");
 
   LOGCFG.prefix = (char *)"test_token";
   LOGCFG.headers = true;
   LOGCFG.level = INFO;
 
   testBackgroundRemoval(boardEmpty_png, frameEmpty_png);
-  testExtractTokenFromFrame(boardEmpty_png, frame_token_bridge_png);
+  testExtractTokenFromFrame(boardEmpty_png, frameEmpty_png, token_red_transparent_bg, 838, 385);
 }
