a8bf46fbcd6c334e4c0487211b976f3c9565ba00
Harald Fielker <harald.fielker@gmail.com>
2021-05-29 20:34:26 +0200

    ,

--- include/detector.hpp
+++ include/detector.hpp
@@ -4,10 +4,13 @@
 #include "log.hpp"
 #include "opencv2/opencv.hpp"
 #include "board.hpp"
+#include "token.hpp"
 
-class Detector {
+class Detector
+{
 public:
     static bool calibrateReferenceFrame(Mat &frame, Board &board);
+    static bool queryTokens(Mat &frame, Board &board, std::vector<const Token *> tokens, std::vector<string> ROIs, std::vector<pair<std::string, std::string>> &result);
 };
 
 #endif
--- src/detector.cpp
+++ src/detector.cpp
@@ -1,7 +1,11 @@
 #include "detector.hpp"
 #include "assetdetection.hpp"
+#include "imagediff.hpp"
+#include "tokenshape.hpp"
+#include "tokencolor.hpp"
 
-bool Detector::calibrateReferenceFrame(Mat &frame, Board &board) {
+bool Detector::calibrateReferenceFrame(Mat &frame, Board &board)
+{
     Log(typelog::INFO) << "Detector calibrateReferenceFrame";
 
     Asset detectedBoard;
@@ -25,6 +29,31 @@ bool Detector::calibrateReferenceFrame(Mat &frame, Board &board) {
             return true;
         }
     }
+
+    return false;
+}
+
+bool Detector::queryTokens(Mat &frame, Board &board,
+                           std::vector<const Token *> tokens, std::vector<string> ROIs, std::vector<pair<std::string, std::string>> &result)
+{
+    Log(typelog::INFO) << "Detector queryTokens";
+
+    Asset detectedBoard;
+    if (!AssetDetection::detectAsset(frame, board.asset, detectedBoard))
+    {
+        Log(typelog::ERR) << "detectedBoard failed";
+        return false;
+    }
+
+    Mat boardImage = detectedBoard.getDefault().image;
+
+    // we might have to apply the histogram of the frameBoardEmpty
+    // to the detected board to avoid unstable colors
+
+    Mat diff = ImageDiff::removeBackground(boardImage, board.frameBoardEmpty);
+    imshow("detectedBoard", detectedBoard.getDefault().image);
+    imshow("diff", diff);
+    waitKey(0);
 
     return false;
 }
